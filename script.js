7de4f439c30d5ea8083713d64f40e3efae07ca4a
MedEA-Telemedicina <testmedeatelemedicina@gmail.com>
2025-05-17 11:02:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ verificaMsg.textContent = 'Verifica in corso...';
 verificaMsg.style.color = '#666';
 
 try {
-const response = await fetch('https://script.google.com/macros/s/AKfycbwi9b8hgDuwdp-Vkr0xgkwjw7KG-8K2Wko1ibo4dQEHiEgRYMJum9\_2o3WdefffjXEpzg/exec');
+const response = await fetch('https://script.google.com/macros/s/AKfycbzovjEeyi8NcBXxujZ1DCDhApwAfEPNUiAvJhGyFUT8o_ChoLHO3jYM5J0I--dObZG_rQ/exec');
 const lista = await response.json();
 
 const normalizza = str =>
@@ -176,7 +176,7 @@ inviaBtn.addEventListener('click', async () => {
     });
   });
 
-  await fetch('https://withered-grass-db6d.testmedeatelemedicina.workers.dev/', {
+  await fetch('https://script.google.com/macros/s/AKfycbzovjEeyi8NcBXxujZ1DCDhApwAfEPNUiAvJhGyFUT8o_ChoLHO3jYM5J0I--dObZG_rQ/exec', {
     method: 'POST',
     body: JSON.stringify(payload),
     headers: {
